b77266c54d1839c88863f5438c170409352f5316
Michael Forney <mforney@mforney.org>
2022-10-28 16:32:11 -0700

    Fix Tos structure
    long is 64-bit in the x86_64 sysv abi, but 32-bit in 9front. Use
    fixed-width integer types instead.

--- tos.h
+++ tos.h
@@ -1,25 +1,21 @@
 typedef struct Tos Tos;
 typedef struct Plink Plink;
 
-typedef unsigned long long uvlong;
-typedef long long vlong;
-typedef unsigned long ulong;
-
 struct Tos {
-	struct			/* Per process profiling */
+	struct				/* Per process profiling */
 	{
-		Plink	*pp;	/* known to be 0(ptr) */
-		Plink	*next;	/* known to be 4(ptr) */
-		Plink	*last;
-		Plink	*first;
-		ulong	pid;
-		ulong	what;
+		Plink		*pp;	/* known to be 0(ptr) */
+		Plink		*next;	/* known to be 4(ptr) */
+		Plink		*last;
+		Plink		*first;
+		uint32_t	pid;
+		uint32_t	what;
 	} prof;
-	uvlong	cyclefreq;	/* cycle clock frequency if there is one, 0 otherwise */
-	vlong	kcycles;	/* cycles spent in kernel */
-	vlong	pcycles;	/* cycles spent in process (kernel + user) */
-	ulong	pid;		/* might as well put the pid here */
-	ulong	clock;
+	uint64_t	cyclefreq;	/* cycle clock frequency if there is one, 0 otherwise */
+	int64_t		kcycles;	/* cycles spent in kernel */
+	int64_t		pcycles;	/* cycles spent in process (kernel + user) */
+	uint32_t	pid;		/* might as well put the pid here */
+	uint32_t	clock;
 	/* top of stack is here */
 };
 
